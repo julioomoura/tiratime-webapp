66993c92400a9abf0ebac189ce89ccb121345a5f
Julio <juliomourajom@gmail.com>
2019-08-01 15:08:53 -0300

    Primeiro Commit

---
+++ main.js
@@ -0,0 +1,62 @@
+jogadoresPorTime = document.getElementById('jogadores-por-time')
+numeroDeJogadores = document.getElementById('numero-de-jogadores')
+botaoAdicionar = document.getElementById('adicionar')
+divAdicionar = document.getElementById('adicionar-jogador')
+botaoSortear = document.getElementById('sortear')
+sorteadosElement = document.getElementById('sorteados')
+
+var jogadores = []
+
+botaoAdicionar.onclick = function () {
+    divAdicionar.innerHTML = ''
+    for (var i = 0; i < numeroDeJogadores.value; i++) {
+        var item = document.createElement('input')
+        var atributo = document.createAttribute('class')
+        atributo.value = 'jogador'
+        item.setAttributeNode(atributo)
+        divAdicionar.appendChild(item)
+    }
+}
+
+botaoSortear.onclick = function(){
+    sorteadosElement.innerHTML = ''
+    elementoJogadores = document.getElementsByClassName('jogador')
+    var novosJogadores = Array.from(elementoJogadores)
+    novosJogadores.forEach(element => {
+        jogadores.push(element.value)
+    });
+    jogadores = shuffle(jogadores)
+    var contador = -1
+    jogadores.forEach(element => {
+        contador++
+        if(contador%jogadoresPorTime.value == 0 || contador == 0){
+            var texto = document.createTextNode("Time")
+            var time = document.createElement('h4')
+            sorteadosElement.appendChild(time)
+            time.appendChild(texto)
+        }
+        var texto = document.createTextNode(element)
+        var jogador = document.createElement('h5')
+        sorteadosElement.appendChild(jogador)
+        jogador.appendChild(texto)
+    });
+}
+
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+  
+    // While there remain elements to shuffle...
+    while (0 !== currentIndex) {
+  
+      // Pick a remaining element...
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex -= 1;
+  
+      // And swap it with the current element.
+      temporaryValue = array[currentIndex];
+      array[currentIndex] = array[randomIndex];
+      array[randomIndex] = temporaryValue;
+    }
+  
+    return array;
+  }
